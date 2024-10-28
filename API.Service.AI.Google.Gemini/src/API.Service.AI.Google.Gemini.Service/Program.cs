4cc270060895c4bd8d3f49e7b310977604deb97a
Tiago Pereira Cardoso <tiagocardosso@gmail.com>
2024-10-28 14:03:31 +0100

    Include AddSwaggerGen

--- API.Service.AI.Google.Gemini/src/API.Service.AI.Google.Gemini.Service/Program.cs
+++ API.Service.AI.Google.Gemini/src/API.Service.AI.Google.Gemini.Service/Program.cs
@@ -1,5 +1,7 @@
 using API.Service.AI.Google.Gemini.Domain.Implementation.Interfaces;
 using API.Service.AI.Google.Gemini.Domain.Implementation.Services;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +9,28 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+	c.EnableAnnotations();
+
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = "API - Service Google Gemini", Version = "v1" });
+
+	c.TagActionsBy(api =>
+	{
+		if (api.GroupName != null)
+		{
+			return new[] { api.GroupName };
+		}
+
+		if (api.ActionDescriptor is ControllerActionDescriptor controllerActionDescriptor)
+		{
+			return new[] { controllerActionDescriptor.ControllerName };
+		}
+
+		throw new InvalidOperationException("Unable to determine tag for endpoint.");
+	});
+	c.DocInclusionPredicate((name, api) => true);
+});
 
 builder.Services.AddScoped<IGoogleGeminiService, GoogleGeminiService>();
 
